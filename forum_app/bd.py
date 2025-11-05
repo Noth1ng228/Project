49247cb98bd031ea5a298f2076ffbd9084f141dc
Noth1ng228 <yakymyk.sergii@gmail.com>
2025-11-05 16:24:55 +0100

    bd.py

--- forum_app/bd.py
+++ forum_app/bd.py
@@ -5,3 +5,9 @@ db = SQLAlchemy()
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(300), nullable=False)
+    replies = db.relationship('Reply', backref='post', cascade="all, delete", lazy=True)
+
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
